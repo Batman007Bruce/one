0527fd9dbcf9cfa63c11058bcc7ea3e63bb51d96
Batman007Bruce <151559682+Batman007Bruce@users.noreply.github.com>
2023-11-21 21:42:23 +0530

    Add files via upload

---
+++ exp2.m
@@ -0,0 +1,26 @@
+% MATLAB code for Forward and 
+% Inverse Fourier Transform
+  
+% FORWARD FOURIER TRANSFORM
+k = imread('cameraman.tif');
+  
+% Apply fourier transformation.
+f=fft2(k);
+  
+% Take magnitude of FT.
+f1=abs(f);
+  
+% Take log of magnitude of FT.
+f2=log(abs(f));
+  
+% Shift FT from corners to central part.
+f3=log(abs(fftshift(f)));
+  
+% Display all three FT images.
+imtool(f1,[]); 
+imtool(f2,[]);
+imtool(f3,[]);
+  
+% Remove some frequency from FT.
+f(1:20, 20:40)=0;
+imtool(log(abs(f)),[]);
